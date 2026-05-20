6f00d9afcd1c6065e3c434b964ee89b65748b876
gibr4nquinho <giovana.branquinho@sptech.school>
2026-05-20 00:13:46 -0300

    ADD

--- ETL/src/main/java/school/sptech/service/S3Service.java
+++ ETL/src/main/java/school/sptech/service/S3Service.java
@@ -7,7 +7,7 @@ import java.io.InputStream;
 
 public class S3Service {
     private final S3Client s3 = S3Config.getS3Client();
-    private final String bucketNome = "easydata";
+    private final String bucketNome = "easydataGiovana12";
 
     public InputStream obterArquivo(String chaveS3) {
         GetObjectRequest request = GetObjectRequest.builder()
