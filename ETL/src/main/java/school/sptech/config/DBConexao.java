42ee1ab4e877dee185e2afe4abda9adcaf684b71
JoaoGomes-Melo <joao.gmelo@sptech.school>
2026-05-20 19:45:52 -0300

    Alterando coneção java jdbc

--- ETL/src/main/java/school/sptech/config/DBConexao.java
+++ ETL/src/main/java/school/sptech/config/DBConexao.java
@@ -1,6 +1,7 @@
 package school.sptech.config;
 
 import org.apache.commons.dbcp2.BasicDataSource;
+
 import javax.sql.DataSource;
 
 public class DBConexao {
@@ -8,12 +9,22 @@ public class DBConexao {
 
     public DBConexao() {
         BasicDataSource ds = new BasicDataSource();
-        ds.setUrl("jdbc:mysql://localhost:3306/EasyData?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true");
-        ds.setUsername("Easy");
-        ds.setPassword("Easydata@2026");
+
+        String url = System.getenv().getOrDefault(
+                "DB_URL",
+                "jdbc:mysql://container-banco:3306/EasyData?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true"
+        );
+
+        String user = System.getenv().getOrDefault("DB_USER", "Easy");
+        String password = System.getenv().getOrDefault("DB_PASSWORD", "Easydata@2026");
+
+        ds.setUrl(url);
+        ds.setUsername(user);
+        ds.setPassword(password);
         ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
         ds.setInitialSize(5);
         ds.setMaxTotal(10);
+
         this.conexao = ds;
     }
 
